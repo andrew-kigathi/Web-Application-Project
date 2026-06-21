a59c9e8a303780bbbf3c3964e4d17c879278165e
andrew-kigathi <andrewkruirie@gmail.com>
2026-06-21 22:14:51 +0300

    Creating Fri folder

---
+++ fri/src/App.tsx
@@ -0,0 +1,11 @@
+import Greetings from "./content";
+
+function App() {
+  return (
+    <div className="App">
+      <Greetings />
+    </div>
+  );
+}
+
+export default App;
---
+++ fri/src/content.tsx
@@ -0,0 +1,6 @@
+function Greetings() {
+    const name = "Andrew";
+    return <h1>Good morning, {name}!</h1>;
+}
+
+export default Greetings;
