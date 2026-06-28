1e256f96e459ce3622cecc5efcedd8bde75ab687
andrew-kigathi <andrewkruirie@gmail.com>
2026-06-28 17:43:29 +0300

    creating other web pages and components in fri folder

---
+++ fri/src/Pages/About.tsx
@@ -0,0 +1,10 @@
+export function About(){
+    return(
+        <>
+            <div className="container mt-5">
+                <h1> About us</h1>
+                <p> Welcome and get to know us </p>
+            </div>
+        </>
+    );
+}
---
+++ fri/src/Pages/home.tsx
@@ -0,0 +1,10 @@
+export function home(){
+    return(
+        <>
+            <div>
+                <h1>Home Page</h1>
+                <p>Welcome to the home page</p>
+            </div>
+        </>
+    );
+}
