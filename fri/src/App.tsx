758210cf99a7207e814c07f8f38b0b180af471b6
andrew-kigathi <andrewkruirie@gmail.com>
2026-06-28 17:42:49 +0300

    changes to fri folder

--- fri/src/App.tsx
+++ fri/src/App.tsx
@@ -1,11 +1,2 @@
-import Greetings from "./content";
-
-function App() {
-  return (
-    <div className="App">
-      <Greetings />
-    </div>
-  );
-}
-
-export default App;
+import { home } from "./Pages/home";
+import { About } from "./Pages/about";
